9347e6759e0b83c2f46827d59ee0010087e9d8c7
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2024-04-24 15:36:58 +0530

    Time: 42 ms (23.34%) | Memory: 16.6 MB (10.49%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,42 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        k=n
+        l=[]
+        
+        while k > 3:
+            if k**2>9:
+
+                h=0
+                
+                for i in str(k) :
+                    h = int(i)**2 + h
+            else:
+                return False
+            k=h
+            print(l)
+            if k in l:
+                return False
+            l.append(h)
+            
+            
+            
+            
+
+        if k == 1:
+            return True
+        else:
+            return False
+
+            
+            
+            
+            
+            
+            
+                
+
+
+
+
+
+        

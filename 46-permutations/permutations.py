177c0d925f25ef541c177441dd2fc5002cd28f85
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2025-02-16 19:54:03 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (94.87%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,7 +1,7 @@
-import itertools
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(itertools.permutations(nums))
-
-
-        
+        if len(nums) == 0:
+            return [[]]
+        else:
+            return [perm[:i] + [nums[0]] + perm[i:] for perm in self.permute(nums[1:]) for i in range(len(perm) + 1)] 

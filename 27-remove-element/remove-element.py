f168de1b0a9d7c1928ff4da49d70e1f3363095d1
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2025-02-23 13:11:15 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.63%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0  # Pointer to track position of non-val elements
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]  # Overwrite element in-place
+                k += 1  # Increase count of non-val elements
+
+        return k  # Return count of valid elements

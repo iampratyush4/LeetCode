4857ce53200dbefbe813246fc1d4cdd62a6f8247
Pratyush Upadhyay <52798350+iampratyush4@users.noreply.github.com>
2025-03-04 00:06:55 +0000

    Time: 2 ms (82.20%) | Memory: 19.2 MB (30.25%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -1,17 +1,23 @@
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#         self.val = val        # The value of the node.
+#         self.left = left      # Pointer to the left child node.
+#         self.right = right    # Pointer to the right child node.
+
+# Solution class which implements the method to convert a sorted array to a balanced binary search tree.
 class Solution:
+    # Function to convert a sorted array into a Binary Search Tree (BST).
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        def midnode(l,r):
-            if l>r:
+        # Helper function to recursively find the middle of the array (or subarray)
+        # and create the tree.
+        def midnode(l, r):
+            if l > r:        # Base case: if the left index exceeds the right index, return None.
                 return None
-            m=(l+r)//2
-            root= TreeNode(nums[m])
-            root.left=midnode(l,m-1)
-            root.right=midnode(m+1,r)
-            return root
-        return midnode(0,len(nums)-1)
+            m = (l + r) // 2 # Find the middle index.
+            root = TreeNode(nums[m])  # Create a new TreeNode with the middle element as its value.
+            root.left = midnode(l, m - 1)  # Recursively construct the left subtree (left part of array).
+            root.right = midnode(m + 1, r)  # Recursively construct the right subtree (right part of array).
+            return root       # Return the created node with left and right children.
+
+        return midnode(0, len(nums) - 1)  # Start the recursion with the entire array range.
